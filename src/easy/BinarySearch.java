bd6cec11c2f00471101f15bf4b5b6038684196ed
Diptendu <diptendu.tu@gmail.com>
2024-03-21 03:02:53 +0530

    Binary Search Updated

--- src/easy/BinarySearch.java
+++ src/easy/BinarySearch.java
@@ -2,28 +2,27 @@ package easy;
 
 public class BinarySearch {
 
-    public static void bSearch(int[] arr, int target){
-        int low=0, high=arr.length-1, mid=0;
-        while (low<high){
+    public static void bSearch(int[] arr, int target) {
+        int low = 0, high = arr.length - 1, mid = 0;
+        while (low <= high) {
 
-            mid = low + (high-low)/2;
+            mid = low + (high - low) / 2;
 
-            if(target == arr[mid]){
+            if (target == arr[mid]) {
                 System.out.println(" Index is: " + mid);
                 break;
             }
 
-            if(target>arr[mid]){
-                low= mid+1;
-            }
-            else{
-                high = mid -1;
+            if (target > arr[mid]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
     }
 
     public static void main(String[] args) {
 
-        bSearch(new int[] {1,2,3,4,5,6,7,8}, 4);
+        bSearch(new int[]{1, 2, 3, 4, 5, 6, 7, 8}, 1);
     }
 }
