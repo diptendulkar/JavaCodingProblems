be72c795de89147ea78b6b142a69f096350840b8
Diptendu <diptendu.tu@gmail.com>
2024-04-08 00:02:17 +0530

    Find Duplicates from String of Array

--- src/coreJava/StreamExample.java
+++ src/coreJava/StreamExample.java
@@ -1,6 +1,7 @@
 package coreJava;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -9,12 +10,12 @@ import java.util.stream.Stream;
 public class StreamExample {
     public static void main(String[] args) {
 
-        //MAP -> One-to-One Transformation: double the number
+        //*****   MAP -> One-to-One Transformation: double the number  *****//
         Stream<Integer> myStream = Stream.of(1,5,2,7,4);
         myStream = myStream.map(a -> a*2);
        // myStream.forEach(System.out::println);
 
-        //FlatMap -> One-to-Many Transformation -> change to char array
+        //*****  FlatMap -> One-to-Many Transformation -> change to char array *****//
         Stream<String> words = Stream.of("hello", "world");
         //conver to char list
         List<Character> characters = words.flatMap(w ->  w.chars().mapToObj(c -> (char) c)).collect(Collectors.toList());
@@ -23,18 +24,34 @@ public class StreamExample {
             return String.CASE_INSENSITIVE_ORDER.compare(e1.toString(), e2.toString());
         }).collect(Collectors.toList());
 
-        // remove duplicate char using distinct method
+        //***** remove duplicate char using distinct method *****//
         characters =   characters.stream().distinct().collect(Collectors.toList());
       //  characters.forEach(System.out::println); // This will print each character on a new line
 
 
-        // JOIN TWO List using Stream
+        //***** JOIN TWO List using Stream *****//
 
         List<String> list1 = Stream.of("Dip", "Das").collect(Collectors.toList());
         List<String> list2 = Stream.of("Diptendu", "Das").collect(Collectors.toList());
 
         List<String> mergeList = Stream.concat(list1.stream(),list2.stream()).collect(Collectors.toList());
-        mergeList.stream().forEach(System.out::println);
+      //  mergeList.stream().forEach(System.out::println);
+
+        // *****  Find Duplicates from String of Array  *****//
+
+        String[] array = {"Dip","Das", "Dip","abc", "xyz", "abc"};
+        List<String> duplicates = new ArrayList<>();
+
+        duplicates = Arrays.stream(array)
+                .collect(Collectors.groupingBy(s->s))
+                .entrySet().stream()
+                .filter(e->e.getValue().size() >1)
+                .map(e-> e.getKey())
+                .collect(Collectors.toList());
+
+        duplicates.stream().forEach(System.out::println);
+
+
     }
 
 }
