01c4860eaadade55ed665268838200a9b64e101c
Diptendu <diptendu.tu@gmail.com>
2024-04-10 21:07:56 +0530

    Multi Threading syncronized

--- src/coreJava/multithreading/MyRunnable.java
+++ src/coreJava/multithreading/MyRunnable.java
@@ -5,9 +5,12 @@ public class MyRunnable implements  Runnable{
     int count =0;
     @Override
     public void run() {
-        for(int i=0; i< 1000; i++){
-            this.count++;
+        synchronized (this){
+            for(int i=0; i< 100000; i++){
+                this.count++;
+            }
         }
+
         System.out.println(Thread.currentThread().getName() + " : " + this.count);
     }
 }
--- src/coreJava/multithreading/MyThread.java
+++ src/coreJava/multithreading/MyThread.java
@@ -9,8 +9,15 @@ public class MyThread {
         // As runnable object are diffrent both threads works independetly on count
         Thread t1 = new Thread(runnable1,"Thread 1");
         Thread t2 = new Thread(runnable2,"Thread 2");
+//        t1.start();
+//        t2.start();
 
-        t1.start();
-        t2.start();
+
+        //SHARED RUNNABLE
+        Thread t3 = new Thread(runnable1,"Thread 1");
+        Thread t4 = new Thread(runnable1,"Thread 2");
+        t3.start();
+        t4.start();
+
     }
 }
