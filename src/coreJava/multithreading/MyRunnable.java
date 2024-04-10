a63f0ef71e25f0a6e8b88e4f73706a3c7c9a81cf
Diptendu <diptendu.tu@gmail.com>
2024-04-11 00:24:07 +0530

    Multi Threading syncronized

--- src/coreJava/multithreading/MyRunnable.java
+++ src/coreJava/multithreading/MyRunnable.java
@@ -2,15 +2,18 @@ package coreJava.multithreading;
 
 public class MyRunnable implements  Runnable{
 
-      int  count =0;
+    volatile  int  count =0;
     @Override
     public void run() {
-        synchronized (this){
-            for(int i=0; i< 10; i++){
+      //  synchronized (this){
+//            for(int i=0; i< 10; i++)
+        while(count <10)
+            {
                 this.count++;
+                System.out.println(Thread.currentThread().getName() + " : " + this.count);
             }
-        }
+       // }
 
-        System.out.println(Thread.currentThread().getName() + " : " + this.count);
+
     }
 }
