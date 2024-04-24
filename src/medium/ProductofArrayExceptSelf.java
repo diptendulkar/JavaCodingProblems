f6efce323e9503b6b198741d2f17f9da7ef4f6f8
Diptendu <diptendu.tu@gmail.com>
2024-04-24 11:57:54 +0530

    ProductofArrayExceptSelf

--- src/medium/ProductofArrayExceptSelf.java
+++ src/medium/ProductofArrayExceptSelf.java
@@ -4,9 +4,9 @@ import java.util.Arrays;
 
 public class ProductofArrayExceptSelf {
 
-    public static int[] product(int[] arr){
+    public static int[] product(int[] nums){
 
-        int n = arr.length;
+       /* int n = arr.length;
         int[] result = new int[n];
         int prefeix=1;
         for(int i =0; i< n; i++)
@@ -16,10 +16,24 @@ public class ProductofArrayExceptSelf {
         for(int i =0; i< n; i++){
             result[i] = prefeix/arr[i];
         }
-        return  result;
+        return  result;*/
+
+        int numsLength = nums.length;
+        int prefixProduct = 1;
+        int suffixProduct = 1;
+        int[] result = new int[numsLength];
+        for(int i = 0; i < numsLength; i++) {
+            result[i] = prefixProduct;
+            prefixProduct *= nums[i];
+        }
+        for(int i = numsLength-1; i >= 0; i--) {
+            result[i] *= suffixProduct;
+            suffixProduct *= nums[i];
+        }
+        return result;
     }
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(product(new int[] {1,2,3,4})));
+        System.out.println(Arrays.toString(product(new int[] {-1,1,0,-3,3})));
     }
 }
