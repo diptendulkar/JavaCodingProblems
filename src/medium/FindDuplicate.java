bad5ccf2297c78102fa325024137d6d54122909a
Diptendu <diptendu.tu@gmail.com>
2024-04-28 23:17:36 +0530

    Find ALL Duplicate

--- src/medium/FindDuplicate.java
+++ src/medium/FindDuplicate.java
@@ -1,6 +1,8 @@
 package medium;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /*Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
 There is only one repeated number in nums, return this repeated number.
@@ -12,6 +14,10 @@ public class FindDuplicate {
     public static void main(String[] args) {
         int[] nums = {4,3,4,1,2};
         System.out.println(findDuplicate(nums));
+
+        //find all duplicates
+        int[] nums2 ={4,3,2,7,8,2,3,1};
+        System.out.println(findAllDuplicates(nums2));
     }
 
     public static int findDuplicate(int[] nums) {
@@ -38,6 +44,23 @@ public class FindDuplicate {
             fast = nums[fast];
         }
         return fast;
+
+    }
+
+    public static List<Integer> findAllDuplicates(int[] nums) {
+        //nt[] nums2 ={4,-3,2,-7,8,2,3,1};
+        List<Integer> result = new ArrayList<>();
+
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            int x = Math.abs(nums[i]);
+            if (nums[x - 1] < 0) {
+                result.add(x);
+            }
+            nums[x - 1] *= -1;
+        }
+
+        return result;
 
     }
 }
