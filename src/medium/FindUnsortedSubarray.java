9ce6ad985d31842bf8f9d4d99f481fadbb5f9404
Diptendu <diptendu.tu@gmail.com>
2024-04-29 10:10:23 +0530

    FindUnsortedSubarray

--- src/medium/FindUnsortedSubarray.java
+++ src/medium/FindUnsortedSubarray.java
@@ -4,9 +4,21 @@ public class FindUnsortedSubarray {
     public static void main(String[] args) {
 
         int[] nums = {2, 6, 4, 8, 10, 9, 15}; // output 5
-        System.out.println(findUnsortedSubarray(nums));
+        System.out.println(findUnsortedSubarray2(nums));
     }
 
+    public static  int findUnsortedSubarray2(int[] nums) {
+        int start = -1, end = -2;
+        int n = nums.length;
+        int max = nums[0], min = nums[n-1];
+        for (int i = 0; i < n; i++) {
+            max = Math.max(max, nums[i]);
+            min = Math.min(min, nums[n-i-1]);
+            if (max > nums[i]) end = i;
+            if (min < nums[n-i-1]) start = n-i-1;
+        }
+        return end - start +1;
+    }
     public static int findUnsortedSubarray(int[] nums) {
 
         int max = Integer.MIN_VALUE;
