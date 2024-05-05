aaef048c12e1b9a23aa5cd7286b5dc48c670f9d2
Diptendu <diptendu.tu@gmail.com>
2024-05-05 09:36:27 +0530

    SortedSquares

---
+++ src/easy/SortedSquares.java
@@ -0,0 +1,29 @@
+package easy;
+
+// https://leetcode.com/problems/squares-of-a-sorted-array/
+
+import java.util.Arrays;
+
+/*Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+Example 1:
+Input: nums = [-4,-1,0,3,10]
+Output: [0,1,9,16,100]
+Explanation: After squaring, the array becomes [16,1,0,9,100].
+        After sorting, it becomes [0,1,9,16,100].
+Example 2:
+Input: nums = [-7,-3,2,3,11]
+Output: [4,9,9,49,121]*/
+public class SortedSquares {
+    public static void main(String[] args) {
+        int[] nums = {-4, -1, 0, 3, 10};
+        System.out.println(Arrays.toString(sortedSquares(nums)));
+    }
+
+    public static int[] sortedSquares(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = nums[i] * nums[i];
+        }
+        Arrays.sort(nums);
+        return nums;
+    }
+}
