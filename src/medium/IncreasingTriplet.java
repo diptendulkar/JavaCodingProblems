439234b3de6b20f72a86f5e646bb0a84eaa478f4
Diptendu <diptendu.tu@gmail.com>
2024-08-02 08:06:21 +0530

    IncreasingTriplet

---
+++ src/medium/IncreasingTriplet.java
@@ -0,0 +1,30 @@
+package medium;
+
+public class IncreasingTriplet {
+    public static void main(String[] args) {
+
+        int[] nums ={20,100,10,12,5,13};
+        System.out.println(increasingTriplet(nums));
+    }
+    public static boolean increasingTriplet(int[] nums) {
+
+        if (nums == null || nums.length < 3) {
+            return false;
+        }
+        int a = Integer.MAX_VALUE;
+        int b = Integer.MAX_VALUE;
+
+        for (int num : nums) {
+            if (num <= a) {
+                a = num;
+            } else if (num <= b) {
+                b = num;
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+
+    }
+}
